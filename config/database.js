4b5e391573e210d31160f9d8be331a1dcea9a0c1
iShinmazinger <mazinger05x72@gmail.com>
2025-10-16 20:04:54 -0500

    commit añadido compatibilidad render

--- config/database.js
+++ config/database.js
@@ -3,16 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    });
 
 export default sequelize;
